ca33ec4c3ec49579ba3108452c97c6b964e95a7a
Tobias Grampp <tobias@grampp.de>
2023-06-25 10:04:48 +0200

    Created Player

---
+++ Player.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The object, the player will control whilst playing the game.
+ * 
+ * Tobias Grampp 
+ * 0.1
+ */
+public class Player extends GameCharacter
+{
+    /**
+     * Act - do whatever the PLAYER wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Player(){
+        super();
+        turn(-90);
+    }
+    public void act()
+    {
+        // Test für noch unklare Prgogrammteile
+    }
+    //Bewegung nach der x-Achse
+}
