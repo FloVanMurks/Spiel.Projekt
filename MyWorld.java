2d3c77c723b87cbd337dd2bd4264278a4fdc838c
FloVanMurks <136998784+FloVanMurks@users.noreply.github.com>
2023-07-03 09:13:04 +0200

    Update world.java

--- MyWorld.java
+++ MyWorld.java
@@ -1,18 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-//This is a wonderful comment 
-
-
-public class MyWorld extends World
-{
-
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
-    public MyWorld()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(720, 576, 1); 
+public class MyWorld extends World {
+    int pixelsOverBottomEdge; // for set up the position of the player over the bottom edge
+    public MyWorld() {
+        super(720, 576, 1);
+        pixelsOverBottomEdge = 50;
+        // Create a new world with 720×576 cells with a cell size of 1x1 pixels.
+        Greenfoot.setSpeed(45);
+        this.setBackground("images/backgroundImage.png");
+        Player player = new Player();
+        addObject(player, 360, 576-pixelsOverBottomEdge); // places the player horizontally in the middle and vertically pixelsOverBottomEdge over the bottom edge
+        player.setImage("images/playerImage.png");
     }
+    
 }
