14c54fb92ddfc52082922097002e24179d73d93a
Tobias234 <137678738+Tobias234@users.noreply.github.com>
2023-07-16 16:24:27 +0200

    Final Version for Presentation by Tobias

--- Enemy.java
+++ Enemy.java
@@ -9,20 +9,32 @@ import java.util.List;
  */
 public class Enemy extends GameCharacter
 {
+    //Every Enemy has his Laser-List. Here the Laser, the Enemy has shot, are stored
     List<Laser> lasers;
     int Xcord;
     int Ycord;
+    //There is one additional laser, to make sure the size of the array (which
+    //sometimes needs to be checked) is never null.
     Laser laser;
     public Enemy(int posX, int posY){
         super(posX, posY);
+        setImage("Gegnershiff1.png");
         Xcord=posX;
         Ycord=posY;
         lasers = new ArrayList<>();
-        Laser laser = new Laser(100,100,true,true);
+        //Here, the laser is put into the array, so that it is never null.
+        //Since the last parameter is true, it can never despawn.
+        laser = new Laser(100,100,true,true);
         lasers.add(laser);
+        //The Enemy needs to turn for the graphic to work.
+        turn(180);
     }
     public void act()
     {
+        //The Enemy will move randomly in one direction every act.
+        //But it will only do so, if it is still relativelyclose to its
+        //position assigned in the start. This will prevent Enemies from
+        //crashing into other enemies.
         double d = Math.random();
         if(d<0.25){
             if(getY() - 1 > Ycord - 3){
@@ -44,25 +56,27 @@ public class Enemy extends GameCharacter
                 goLeft(1);
             }
         }
-        if(lasers.size() > 0){
+        //Lasers can delete themselves off the world, but to save performance,
+        //they also get removed from the list, if they touched the end of the
+        //screen or an Enemy
             for(int i = 0; i < lasers.size(); i++){
-                Laser laser = lasers.get(i);
-                if(laser.getIsDead()){
-                    lasers.remove(i);
-                }
+            if(lasers.get(i).getIsDead()){
+                lasers.remove(i);
             }
         }
-    }
-    //Methods only for testingreasons
-    public int getXcord(){
-        return Xcord;
-    }
-    public int getYcord(){
-        return Ycord;
+        //The Enemy will than shoot.
+        shoot();
     }
     public void shoot(){
-        if(Greenfoot.isKeyDown("Space")){
-            Laser laser = new Laser(getX(), getY(), true, false);
+        //When getting the order shoot(), the Enemy has a chance of 0.1% of
+        //shooting. Othervise the game would be unplayable.
+        double d = Math.random();
+        if(d < 0.001){
+            //If the Enemy decides to shoot, it will spawn a laser. It starts
+            //at the Enemies exact position. Since the 3rd parameter is false,
+            //it will not hurt Enemies. But it will despawn, since it has false
+            //as its last parameter.
+            Laser laser = new Laser(getX(), getY(), false, false);
             lasers.add(laser);
             ((MyWorld)getWorld()).addObject(laser,getX(), getY());
         }
