835c363759d5056dfd1d23164cb76569c14f54a1
Tobias234 <137678738+Tobias234@users.noreply.github.com>
2023-07-16 16:24:56 +0200

    Final Version for Presentation by Tobias

--- GameCharacter.java
+++ GameCharacter.java
@@ -1,42 +1,39 @@
 import greenfoot.*;
 /**
- * Write a description of class GameCharacter here.
+ * The superclass of Enemies, lasers and the player. Here, some methods needed
+ * everywhere are defined.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
 public class GameCharacter extends Actor{
+    //constructor, wherein x-Position and y-Position are demanded, so that it's
+    //location is set there.
     public GameCharacter(int posX, int posY){
         super();
         setLocation(posX, posY);
-        turn(180);
     }
-    //Bewegung nach der y-Achse
+    //Movement is getting checked, whether the moving object would go out of the picture. Movement will only commence, if this isn't the case.
+    //Movement on the y-Axis
     public void goUp(int movesize){
-        if(getX()>2){
-            turn(90);
-            move(movesize);
-            turn(-90);
+        if(getX()>movesize){
+            setLocation(getX(), getY() - movesize);
         }
     }
     public void goDown(int movesize){
-        if(getX()<718){
-            turn(-90);
-            move(movesize);
-            turn(90);
+        if(getX()<720 - movesize){
+            setLocation(getX(), getY() + movesize);
         }
     }
-    //Bewegung nach der x-Achse
+    //Movement on the x-Axis
     public void goLeft(int movesize){
-        if(getY() < 576){
-            move(movesize);
+        if(getY() < 578 - movesize){
+            setLocation(getX() - movesize, getY());
         }
     }
     public void goRight(int movesize){
-        if(getY() >1 ){
-            turn(180);
-            move(movesize);
-            turn(180);
+        if(getY() > movesize ){
+            setLocation(getX() + movesize, getY());
         }
     }
 }
